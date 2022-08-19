aa2527dbd6b7ca2d23a502d7aee09df9380df614
OtienohOwinoh <coowinoh@gmail.com>
2022-08-19 20:32:04 +0300

    Complete Assignement

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function receivesAFunction(callFunction) {
+    callFunction();
+  }
+  function returnsANamedFunction() {
+    return function ruth() {};}
+
+    function returnsAnAnonymousFunction() {
+        return function () {};
+      }
